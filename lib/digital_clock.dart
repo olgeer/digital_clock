d60dc27428d52464c48557d2394b99270c0f4eb1
max <olgeer@163.com>
2021-04-21 15:11:16 +0800

    修正了一些合并遗漏的问题

--- lib/digital_clock.dart
+++ lib/digital_clock.dart
@@ -2,7 +2,7 @@ library digital_clock;
 
 export './src/digitalClock.dart';
 export './src/flipNumber.dart';
-// export './src/alarm_clock.dart';
+export './src/alarm_clock.dart';
 export './src/vibrate.dart';
 export './src/lamp.dart';
 export './src/sound.dart';

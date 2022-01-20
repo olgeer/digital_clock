666b699150f4c9a0eece28d53923708b41e11118
max <olgeer@163.com>
2022-01-20 16:19:50 +0800

    分离基础功能模块；

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:console/console.dart';
+import 'package:base_utility/base_utility.dart';
 import 'package:digital_clock/digital_clock.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
@@ -115,38 +115,6 @@ class ClockComponentState extends State<ClockComponent> {
   void dispose(){
     myTimer.cancel();
     super.dispose();
-  }
-
-  void initLogger({Level logLevel=Level.FINE}){
-    Logger.root.level = logLevel;
-    Logger.root.onRecord.listen((event) {
-      String color="{@yellow}";
-      final String colorEnd="{@end}";
-      switch(event.level.value){
-        case 0:
-        case 300:
-        case 400:
-        case 500:
-          color="{@green}";
-          break;
-        case 700:
-        case 800:
-          color="{@blue}";
-          break;
-        case 900:
-          color="{@magenta}";
-          break;
-        case 1000:
-          color="{@cyan}";
-          break;
-        case 1200:
-        default:
-          color="{@yellow}";
-          break;
-      }
-      if(event.level>=logLevel)print(format("${DateTime.now().toString()} - {@blue}[${event.loggerName}]{@end} - $color${event.level.toString()}{@end} : ${event.message}"));
-      // log("${DateTime.now().toString()} -- ${event.level.toString()} : ${event.message}",time:DateTime.now(),name: event.loggerName,level: 0);
-    });
   }
 
   void tt(_){
--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import soundpool_macos
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SwiftSoundpoolPlugin.register(with: registry.registrar(forPlugin: "SwiftSoundpoolPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
